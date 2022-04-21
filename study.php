65abb79e4ce0a7f537ae5dbad055e4657158f72d
Anton Stupin <99168244+inputsAn@users.noreply.github.com>
2022-04-22 03:11:19 +0500

    Add files via upload

---
+++ study.php
@@ -0,0 +1,23 @@
+<?php include_once "template/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Учеба</title>
+    <link rel="stylesheet" href="/styles/styles.css">
+    <meta name="viewpoint" content="width=device-width">
+</head>
+<body>
+<div class ="main_content">
+        <div class="labs">
+            <h2 align="center">Лабораторные работы по МЛиДМ:</h2>
+            <ol>
+                <li>Первая лабораторная работа</li>
+                <li>Вторая лабораторная работа</li>
+                <li>Третья лабораторная работа</li>
+                <li>Четвертая лабораторная работа</li>
+                <li>Пятая лабораторная работа</li>
+            </ol>
+        </div>
+</div>
+
